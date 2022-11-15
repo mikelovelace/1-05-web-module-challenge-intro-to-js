9d3487b3afaeab0bb6d94c0130b2bf691518f657
mikelovelace <miketlovelace@gmail.com>
2022-11-15 15:14:02 -0500

    finished task 3

--- index.js
+++ index.js
@@ -140,9 +140,26 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if(age >= 1 && weight > 15) {
+    return weight * 0.02
+  } else if(age >= 1 && weight <= 15 && weight >= 11) {
+    return weight * 0.03
+  } else if(age >= 1 && weight <= 10 && weight >= 6) {
+    return weight * 0.04
+  } else if(age >= 1 && weight <= 5) {
+    return weight * 0.05
+  } else if(age <= 0.333) {
+    return weight * 0.10
+  } else if(age > 0.333 && age <= 0.583) {
+    return weight * 0.05
+  } else if(age > 0.583 && age < 1) {
+    return weight * 0.04
+  } else {
+    return `Your dog does not exist yet`
+  }
 }
+console.log(hungryDog(15, 1))
 
 
 
